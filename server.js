50b16df37314c72794cd4c84769ebc871ffa1758
szt2-04 <szt2-04@szlgbp.hu>
2021-05-31 13:06:55 +0200

    post

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+//-----------------
 
-
+app.use(express.urlencoded());
 app.use(express.static('public'));
 
+//----------------------
 
+app.post('/szavazas',function(request, response){
+    console.log(request.body);
+    response.redirect('/');
+});
+
+//--------------------
 app.listen(9000);
