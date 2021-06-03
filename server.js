ffc1f28dc05cb670f2a52ea74c0631f88c26d910
szt2-04 <szt2-04@szlgbp.hu>
2021-06-03 11:45:45 +0200

    Database hozzáadás, schema, szavazat

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.json');
+
+
+mongoose.connect(config.databaseURL);
+//
+
 const app = express();
 
 //-----------------
 
+const schema = new mongoose.Schema({
+    nev : String,
+    szavazatok: Number
+});
+
+const model = mongoose.model('Opciok', schema ,'Opciok');
+
+
 app.use(express.urlencoded());
 app.use(express.static('public'));
+
+
+
+
 
 //----------------------
 
 app.post('/szavazas',function(request, response){
     console.log(request.body);
+
+    model.findOne({ nev : request.body.teszta}, function(err,doc){
+        if(doc){
+            console.log(request.body.teszta + 'már létezik');
+            //változtatjuk
+            doc.szavazatok++;
+            doc.save();
+        } else {
+            new model({
+                nev: request.body.teszta,
+                szavazatok: 1
+            }).save();
+        }
+    });
+
+
+    
+
+    
+
     response.redirect('/');
 });
 
